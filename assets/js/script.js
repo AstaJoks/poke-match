0f85b8ab7486e26b7ee9814acf75a7573866534f
AstaJoks <asta.judickaite@gmail.com>
2023-03-01 22:46:44 +0000

    Add general variables and event listeners for document loading and button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+/**
+ * GENERAL VARIABLES
+ */
+ const cards = document.querySelectorAll('.memory-card');
+ const moveContainer = document.querySelector(".moves");
+ const modal = document.getElementById('modal');
+ const timeContainer = document.querySelector(".time");
+ const MAX_MATCH = 8;
+ const button = document.querySelector("button");
+
+ 
+ let gameOn = false;
+ let perfectMatch = 0;
+ let flippedCard = false; //used to check if card has already been clicked 
+ let lockBoard = false; // used to lock the board until each set of cards are finished are finished before selecting the next two
+ let firstCard, secondCard; //Used to check for cards match
+ let moves = 0;
+ let finalTime = "";
+ 
+ //events
+ cards.forEach(card => card.addEventListener('click', flipCard)); // listen for card flips
+ shuffle();
+
+
 function flipCard () {
 
 }
