edec71cc4151b2c6d70f37f5b61b3dc015a2025b
AstaJoks <asta.judickaite@gmail.com>
2023-03-01 19:39:42 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function flipCard () {
+
+}
+
+function checkCardMatch () {
+
+}
+
+function pairMatch () {
+
+}
+
+function checkNoMatch () {
+
+}
+
+function timer () {
+
+}
+
+function cstopTime () {
+
+}
+
+function addMove () {
+
+}
+
+function resetGame () {
+
+}
+
+function winGame () {
+
+}
+
+function reset () {
+
+}
